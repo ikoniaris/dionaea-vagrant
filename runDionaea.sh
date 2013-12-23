557a3c8795794ecc5230605b881735c5c5a76305
Ion <ikoniaris@gmail.com>
2013-12-23 13:19:13 +0200

    Add sudo to runDionaea for cronjob as vagrant user

--- runDionaea.sh
+++ runDionaea.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Start p0f
-p0f -i eth1 -Q /tmp/p0f.sock -l -d -o /var/log/p0f.log
+sudo p0f -i eth1 -Q /tmp/p0f.sock -l -d -o /var/log/p0f.log
 
 #Start Dionaea as daemon
-/opt/dionaea/bin/dionaea -D -l all,-debug -L '*' -c /opt/dionaea/etc/dionaea/dionaea.conf -p /opt/dionaea/var/run/dionaea.pid
+sudo /opt/dionaea/bin/dionaea -D -l all,-debug -L '*' -c /opt/dionaea/etc/dionaea/dionaea.conf -p /opt/dionaea/var/run/dionaea.pid
